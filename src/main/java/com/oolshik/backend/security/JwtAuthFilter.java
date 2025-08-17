4a25123ae7b7841ab2312373a22b6985777f355d
Nitin Kalokhe <nitinkalokhe@Nitins-MacBook-Pro.local>
2025-08-17 19:05:57 +0530

    basic fix version working

--- src/main/java/com/oolshik/backend/security/JwtAuthFilter.java
+++ src/main/java/com/oolshik/backend/security/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,7 +21,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService uds;
 
-    public JwtAuthFilter(JwtService jwtService, UserDetailsService uds) {
+    public JwtAuthFilter(JwtService jwtService, @Lazy UserDetailsService uds) {
         this.jwtService = jwtService;
         this.uds = uds;
     }
